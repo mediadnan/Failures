cdead974550dd52d7e2c2ce3a960ee2285b64d9f
MARSO Adnan <mediadnan@gmail.com>
2023-04-01 13:52:34 +0000

    added helper methods sources and errors

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,6 +12,14 @@ class FailureHolder:
     def __call__(self, source: str, error: Exception) -> None:
         self.failures.append((source, error))
 
+    @property
+    def sources(self) -> List[str]:
+        return [src for src, _ in self.failures]
+
+    @property
+    def errors(self) -> List[Exception]:
+        return [failure for _, failure in self.failures]
+
 
 @fixture
 def handler():
