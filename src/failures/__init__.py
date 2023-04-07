1c8a168f24eaac53cc2ff15076a552b88376eb00
MARSO Adnan <mediadnan@gmail.com>
2023-04-07 11:12:30 +0000

    Bump to 0.2.0

---
+++ src/failures/__init__.py
@@ -0,0 +1,21 @@
+"""Failures package provides tools to label and track errors anywhere in your code easily"""
+from .plugins import handlers, default_handler
+from .elements import handler, scope, FailureHandler, Failure
+from ._legacy import handle
+
+
+# -------------------------------- WARNING --------------------------------- #
+#                                                                            #
+#       Anything that starts with an _ or not explicitly imported here       #
+#       or mentioned anywhere in the documentation is considered             #
+#       internal API and implementation details                              #
+#       and could potentially change in minor/patch versions                 #
+#       USING PRIVATE MEMBERS MAY POTENTIALLY BREAK                          #
+#       YOUR APP IN ANY NEXT RELEASE,                                        #
+#       and the package does not commit to keep the                          #
+#       internal API compatibility for the next patch/minor version.         #
+#                                                                            #
+# -------------------------------------------------------------------------- #
+
+
+__version__ = '0.2.0'
