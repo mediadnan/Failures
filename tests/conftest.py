283cb8033b6f9a1d5145a680e4ecbddd7abbd520
MARSO Adnan <mediadnan@gmail.com>
2023-04-09 16:16:02 +0000

    added label variations to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,9 @@
-from typing import Tuple, List
-
-from pytest import fixture
+import typing
+import pytest
 
 
 class FailureHolder:
-    failures: List[Tuple[str, Exception]]
+    failures: typing.List[typing.Tuple[str, Exception]]
 
     def __init__(self):
         self.failures = []
@@ -13,19 +12,43 @@ class FailureHolder:
         self.failures.append((source, error))
 
     @property
-    def sources(self) -> List[str]:
+    def sources(self) -> typing.List[str]:
         return [src for src, _ in self.failures]
 
     @property
-    def errors(self) -> List[Exception]:
+    def errors(self) -> typing.List[Exception]:
         return [failure for _, failure in self.failures]
 
 
-@fixture
+@pytest.fixture
 def handler():
     return FailureHolder()
 
 
-@fixture
+@pytest.fixture
 def error():
     return Exception("Test error")
+
+
+def pytest_generate_tests(metafunc):
+    """Automatically parametrizes tests for common tests"""
+    if "valid_label" in metafunc.fixturenames:
+        metafunc.parametrize("valid_label", [
+            "root",
+            "root.sub",
+            "root.sub_scope",
+            "root.sub.sub.sub",
+            "root.scope1",
+            "scope2",
+            "scope.iteration[5].func",
+            "scope.func(1)",
+        ])
+    if all(fixt in metafunc.fixturenames for fixt in ("invalid_label", "err_type", "err_msg")):
+        metafunc.parametrize("invalid_label,err_type,err_msg", [
+            pytest.param(object(), TypeError, "label must be a string", id="wrong_type_name"),
+            pytest.param(b'name.sub', TypeError, "label must be a string", id="wrong_type_name_bytes"),
+            pytest.param('', ValueError, "invalid label: ''", id="empty_name"),
+            pytest.param('name..sub', ValueError, "invalid label: 'name..sub'", id="double_dot"),
+            pytest.param('.name', ValueError, "invalid label", id="leading_dot"),
+            pytest.param('name.', ValueError, "invalid label", id="trailing_dot"),
+        ])
