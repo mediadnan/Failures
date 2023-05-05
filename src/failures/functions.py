798a3e6baf9ab9fe22158d20143e708a64986aea
Adnan MARSO <mediadnan@gmail.com>
2023-05-05 13:09:31 +0100

    MyPy ...

--- src/failures/functions.py
+++ src/failures/functions.py
@@ -1,16 +1,17 @@
+from __future__ import annotations
 import types
 import inspect
 import functools
-from typing import Callable, Any, Optional, Union, Dict, Tuple, overload
+from typing import Callable, Any, Optional, Union, Dict, Tuple, overload, cast, Awaitable
 
 from typing_extensions import get_origin, get_args
 
-from .core import Reporter, P, T, FunctionVar, _invalid
+from .core import Reporter, P, T, _invalid
 
 REPORTER_ARG_NAME = 'reporter'
 NOTSET = object()
 
-_union_types = [Union]
+_union_types: list = [Union]
 try:
     _union_types.append(types.UnionType)
 except AttributeError:
@@ -21,7 +22,7 @@ def _is_param_reporter(name: str, annotation: Any, default: Any) -> bool:
     _type_hinted_reporter = (annotation is Reporter or
                              (get_origin(annotation) in _union_types and
                               Reporter in get_args(annotation)))
-    _name_hinted_reporter = name == 'reporter' and annotation is NOTSET
+    _name_hinted_reporter = name == REPORTER_ARG_NAME and annotation is NOTSET
     _right_default = default is NOTSET or default is None or isinstance(default, Reporter)
     return (_type_hinted_reporter or _name_hinted_reporter) and _right_default
 
@@ -36,60 +37,68 @@ def _get_set_reporter_from_signature(func: Callable, /) -> Tuple[
     # Check if positional arguments contain the reporter
     for idx, _arg in enumerate(spec.args):
         try:
-            default = spec.defaults[idx - len(spec.args)]
+            default = spec.defaults[idx - len(spec.args)]  # type: ignore
         except (IndexError, TypeError):
             default = NOTSET
         if not _is_param_reporter(_arg, spec.annotations.get(_arg, NOTSET), default):
             continue
 
-        def _get(args: tuple, _kwargs: Dict[str, Any]) -> Optional[Reporter]:
+        def _get(_args: tuple, _kwargs: Dict[str, Any], /) -> Optional[Reporter]:
             try:
-                return args[idx]
+                return _args[idx]
             except IndexError:
                 if default is not NOTSET:
                     return default
                 raise _invalid(TypeError, f"{name}() is missing the reporter as required positional argument") from None
 
-        def _set(args: tuple, kwargs: Dict[str, Any], reporter: Reporter) -> Tuple[tuple, Dict[str, Any]]:
-            _args = list(args)
+        def _set(_args: tuple, _kwargs: Dict[str, Any], _reporter: Reporter, /) -> Tuple[tuple, Dict[str, Any]]:
+            args = list(_args)
             try:
-                _args[idx] = reporter
+                args[idx] = _reporter
             except IndexError:
-                _args.append(reporter)
-            return tuple(_args), kwargs
+                args.append(_reporter)
+            return tuple(args), _kwargs
 
         return _get, _set
 
     # Check if keyword arguments contain the reporter
     for _arg in spec.kwonlyargs:
         try:
-            default = spec.kwonlydefaults[_arg]
+            default = spec.kwonlydefaults[_arg]  # type: ignore  # Brutal access
         except (KeyError, TypeError):
             default = NOTSET
         if not _is_param_reporter(_arg, spec.annotations.get(_arg, NOTSET), default):
             continue
-        if default is NOTSET:
-            default = None
 
-        def _get(_args: tuple, kwargs: Dict[str, Any]) -> Optional[Reporter]:
+        def _get(_args: tuple, _kwargs: Dict[str, Any], /) -> Optional[Reporter]:
             try:
-                return kwargs[_arg]
+                return _kwargs[_arg]
             except KeyError:
                 if default is not NOTSET:
                     return default
                 raise _invalid(TypeError, f"{name}() is missing the reporter as required keyword argument") from None
 
-        def _set(args: tuple, kwargs: Dict[str, Any], reporter: Reporter) -> Tuple[tuple, Dict[str, Any]]:
-            kwargs[_arg] = reporter
-            return args, kwargs
+        def _set(_args: tuple, _kwargs: Dict[str, Any], _reporter: Reporter, /) -> Tuple[tuple, Dict[str, Any]]:
+            _kwargs[_arg] = _reporter
+            return _args, _kwargs
 
         return _get, _set
 
     # Return default 'get' and 'set' functions
-    return lambda _a, _k: None, lambda a, k, _: (a, k)
+    def _get(_args: tuple, _kwargs: Dict[str, Any], /) -> Optional[Reporter]:  # type: ignore[no-redef]
+        return None
+
+    def _set(  # type: ignore[no-redef]
+            _args: tuple,
+            _kwargs: Dict[str, Any],
+            _reporter: Reporter, /
+    ) -> Tuple[tuple, Dict[str, Any]]:
+        return _args, _kwargs
+
+    return _get, _set
 
 
-def is_async(func: FunctionVar, /) -> bool:
+def is_async(func: Callable, /) -> bool:
     while isinstance(func, functools.partial):
         func = func.func
     return inspect.iscoroutinefunction(func) or (
@@ -98,16 +107,16 @@ def is_async(func: FunctionVar, /) -> bool:
     )
 
 
-def get_func_name(func: FunctionVar, /) -> str:
+def get_func_name(func: Callable, /) -> str:
     while isinstance(func, functools.partial):
         func = func.func
     return func.__name__
 
 
 @overload
-def scoped(func: FunctionVar, /) -> FunctionVar: ...
+def scoped(func: Callable[P, T], /) -> Callable[P, T]: ...
 @overload
-def scoped(name: str = ..., /) -> Callable[[FunctionVar], FunctionVar]: ...
+def scoped(name: str = ..., /) -> Callable[[Callable[P, T]], Callable[P, T]]: ...
 
 
 def scoped(arg=None, /):
@@ -128,27 +137,29 @@ def scoped(arg=None, /):
     ... def function(*args, **kwargs):
     ...     pass
     """
-    def decorator(func_: FunctionVar, /, name: str = None) -> FunctionVar:
+    def decorator(func_: Callable[P, T], /, name: str = None) -> Callable[P, T]:
         _get, _set = _get_set_reporter_from_signature(func_)
         name_ = name or get_func_name(func_)
         if is_async(func_):
-            async def wrap(*args: P.args, **kwargs: P.kwargs) -> Optional[T]:
+            @functools.wraps(func_)
+            async def wrap(*args: P.args, **kwargs: P.kwargs) -> T:
                 reporter = (_get(args, kwargs) or Reporter)(name_)
                 if not isinstance(reporter, Reporter) or reporter is None:
                     raise _invalid(TypeError, f"The reporter got wrong type {type(reporter)!r}")
-                args, kwargs = _set(args, kwargs, reporter)
+                _args, _kwargs = _set(args, kwargs, reporter)
                 with reporter:
-                    return await func_(*args, **kwargs)
+                    return await cast(Callable[P, Awaitable[T]], func_)(*_args, **_kwargs)
         else:
-            def wrap(*args: P.args, **kwargs: P.kwargs) -> Optional[T]:
+            @functools.wraps(func_)
+            def wrap(*args: P.args, **kwargs: P.kwargs) -> T:
                 reporter = (_get(args, kwargs) or Reporter)(name_)
                 if not isinstance(reporter, Reporter) or reporter is None:
                     raise _invalid(TypeError, f"The reporter got wrong type {type(reporter)!r}")
-                args, kwargs = _set(args, kwargs, reporter)
+                _args, _kwargs = _set(args, kwargs, reporter)
                 with reporter:
-                    return func_(*args, **kwargs)
-        wrap.__signature__ = inspect.signature(func_)
-        return functools.update_wrapper(wrap, func_)
+                    return func_(*_args, **_kwargs)
+        setattr(wrap, '__signature__', inspect.signature(func_))
+        return cast(Callable[P, T], wrap)
     if arg is None:  # In case it was used with emtpy parenthesis @scope()
         return decorator
     elif callable(arg):  # In case it was used without parenthesis @scope
