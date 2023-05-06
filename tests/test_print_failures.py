d68f831cb294003f7585b3295c1b820a726c1fda
MARSO Adnan <mediadnan@gmail.com>
2023-05-06 18:14:21 +0100

    restored mock missing colorama

--- tests/test_print_failures.py
+++ tests/test_print_failures.py
@@ -18,11 +18,11 @@ def test_print_failure(capsys, failure):
                     capsys.readouterr().out)
 
 
-# def test_print_failure_without_colorama(capsys, failure):
-#     import failures
-#     with patch.dict(sys.modules, {'colorama': None}):
-#         importlib.reload(failures.handler)
-#         failures.print_failure(failure)
-#         assert re.match(r"\[FAILURE] root\.target :: Exception\(Test error\) \d{4}-\d\d-\d\d \d\d:\d\d:\d\d",
-#                         capsys.readouterr().out)
-#     importlib.reload(failures.handler)
+def test_print_failure_without_colorama(capsys, failure):
+    import failures
+    with patch.dict(sys.modules, {'colorama': None}):
+        importlib.reload(failures.handler)
+        failures.print_failure(failure)
+        assert re.match(r"\[FAILURE] root\.target :: Exception\(Test error\) \d{4}-\d\d-\d\d \d\d:\d\d:\d\d",
+                        capsys.readouterr().out)
+    importlib.reload(failures.handler)
